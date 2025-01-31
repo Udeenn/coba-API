2dc1c327bcda63f04652dc5f468f22fa4ae2c7ca
Ammar Miftahudin Anshori <ammarmiftah.06@gmail.com>
2025-01-31 09:53:09 +0700

    menambahkan api delete dan create

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Log;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
+
 
 class UserController extends Controller
 {
@@ -18,8 +22,34 @@ class UserController extends Controller
         return response()->json(User::findOrFail($id));
     }
 
-    public function store($request){
-        $user = User::create($request->all());
-        return response()->json($user, 201);
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json(['message' => 'User created successfully', 'user' => $user], 201);
+    }
+
+    public function destroy($id){
+        $user = User::find($id);
+        if(!$user){
+            return response()->json(['message'=>'user not found'], 404);
+        }
+
+        $user->delete();
+
+        return response()->json(['message'=>'user delete successfully'], 200);
     }
 }
